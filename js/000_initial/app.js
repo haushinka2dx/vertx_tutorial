083e86a856e0cdb9fa2a83dfcbc6fd4cf7a25ac8
haushinka2dx <hirano.kei1@gmail.com>
2013-01-03 01:04:31 +0900

    stop7 and step8 done.

--- js/000_initial/app.js
+++ js/000_initial/app.js
@@ -6,6 +6,10 @@ var webServerConf = {
 	bridge : true,
 
 	inbound_permitted : [
+		// Allow calls to login and authorize
+		{
+			address : 'vertx.basicauthmanager.login'
+		},
 		{
 			address : 'vertx.mongopersistor',
 			match : {
@@ -21,6 +25,9 @@ var webServerConf = {
 vertx.deployModule('vertx.mongo-persistor-v1.2', null, 1, function() {
 	load('static_data.js');
 });
+
+// Deploy an auth manager to handle the authentication
+vertx.deployModule('vertx.auth-mgr-v1.1');
 
 // Start the web server, with the config we defined above
 vertx.deployModule('vertx.web-server-v1.0', webServerConf);
