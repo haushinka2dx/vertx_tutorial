3cce2aebf7faec0ec5b5d6601a1d4f96b3380a9f
haushinka2dx <hirano.kei1@gmail.com>
2013-01-02 09:12:07 +0900

    Step4 using mond-persistor-1.2 was done.

--- js/000_initial/app.js
+++ js/000_initial/app.js
@@ -5,5 +5,8 @@ var webServerConf = {
 	host: 'localhost'
 };
 
+// Start a MongoDB persistor module
+vertx.deployModule('vertx.mongo-persistor-v1.2');
+
 // Start the web server, with the config we defined above
 vertx.deployModule('vertx.web-server-v1.0', webServerConf);
